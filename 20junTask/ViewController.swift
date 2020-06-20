d75257fe2b5121f3a4ef920b320825fe970f8c86
Ananda <ananda.aiwale@kidzrouter.com>
2020-06-20 08:28:42 +0530

    Initial Commit

---
+++ 20junTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20junTask
+//
+//  Created by Ananda  on 20/06/20.
+//  Copyright © 2020 Wiz_Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
